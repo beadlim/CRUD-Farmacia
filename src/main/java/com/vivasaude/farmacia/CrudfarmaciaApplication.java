a3aedb47362cdfa67acb6736420307dc5786a9bb
beadlim <beatrizdlim53@gmail.com>
2023-10-19 14:41:43 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/vivasaude/farmacia/CrudfarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.vivasaude.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudfarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudfarmaciaApplication.class, args);
+	}
+
+}
